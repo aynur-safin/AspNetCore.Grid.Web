686dfe3dc7fc47ae34ca5828c18c661cf8987b23
Muchiachio <aldis.rameika@gmail.com>
2015-12-13 19:40:03 +0200

    Grid export example
    - Added grid export example.

--- src/Mvc.Grid.Web/Controllers/GridController.cs
+++ src/Mvc.Grid.Web/Controllers/GridController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
 using NonFactors.Mvc.Grid.Web.Context;
 using NonFactors.Mvc.Grid.Web.Models;
+using OfficeOpenXml;
+using System;
 using System.Linq;
 
 namespace NonFactors.Mvc.Grid.Web.Controllers
@@ -77,6 +80,64 @@ namespace NonFactors.Mvc.Grid.Web.Controllers
         public ActionResult Localization()
         {
             return View(PeopleRepository.GetPeople());
+        }
+
+        [HttpGet]
+        public ActionResult Export()
+        {
+            return View(CreateExportableGrid());
+        }
+
+        [HttpGet]
+        public ActionResult GetExport()
+        {
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                Int32 row = 2;
+                Int32 col = 1;
+                package.Workbook.Worksheets.Add("Data");
+                IGrid<PersonModel> grid = CreateExportableGrid();
+                ExcelWorksheet sheet = package.Workbook.Worksheets["Data"];
+
+                foreach (IGridColumn column in grid.Columns)
+                {
+                    sheet.Cells[1, col].Value = column.Title;
+                    sheet.Column(col++).Width = 18;
+                }
+
+                foreach (var gridRow in grid.Rows)
+                {
+                    col = 1;
+                    foreach (IGridColumn column in grid.Columns)
+                        sheet.Cells[row, col++].Value = column.ValueFor(gridRow);
+
+                    row++;
+                }
+
+                return File(package.GetAsByteArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Export.xlsx");
+            }
+        }
+
+        private IGrid<PersonModel> CreateExportableGrid()
+        {
+            IGrid<PersonModel> grid = new Grid<PersonModel>(PeopleRepository.GetPeople());
+            grid.ViewContext = new ViewContext { HttpContext = HttpContext };
+            grid.Query = Request.Query;
+
+            grid.Columns.Add(model => model.Name).Titled("Name");
+            grid.Columns.Add(model => model.Surname).Titled("Surname");
+
+            grid.Columns.Add(model => model.Age).Titled("Age");
+            grid.Columns.Add(model => model.Birthday).Titled("Birth date");
+            grid.Columns.Add(model => model.IsWorking).Titled("Employed");
+
+            foreach (IGridColumn column in grid.Columns)
+            {
+                column.IsFilterable = true;
+                column.IsSortable = true;
+            }
+
+            return grid;
         }
     }
 }
