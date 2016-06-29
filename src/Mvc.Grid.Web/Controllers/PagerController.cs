9057599cb8539048f28849893f60a5e768f97f77
Muchiachio <aldis.rameika@gmail.com>
2016-06-29 14:30:07 +0300

    Selectable rows per page
    - Added selectable rows per page example.

--- src/Mvc.Grid.Web/Controllers/PagerController.cs
+++ src/Mvc.Grid.Web/Controllers/PagerController.cs
@@ -6,6 +6,12 @@ namespace NonFactors.Mvc.Grid.Web.Controllers
     public class PagerController : Controller
     {
         [HttpGet]
+        public ActionResult SelectableRowsPerPage()
+        {
+            return View(PeopleRepository.GetPeople());
+        }
+
+        [HttpGet]
         public ActionResult PagesToDisplay()
         {
             return View(PeopleRepository.GetPeople());
